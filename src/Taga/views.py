bcdf9b638fd29fd0dc1e1b5fc66fe65fad243b0c
lucaschevalier <lucas.chevalier@lyceeastier.com>
2022-02-08 17:30:02 +0100

    commit

--- src/Taga/views.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-from django.shortcuts import redirect
-
-
-def index(request):
-    return redirect('notation-index')
--- src/notation/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from notation.models import NotationTable, NotesCategories
-
-admin.site.register(NotationTable),
-admin.site.register(NotesCategories),
